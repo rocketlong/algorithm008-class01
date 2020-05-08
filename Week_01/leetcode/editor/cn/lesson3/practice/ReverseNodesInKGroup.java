05fbba89d8f510af46e408fae331355fce12861c
liyunlong <liyunlong@tuzhanai.com>
2020-05-08 13:58:35 +0800

    翻转链表

--- Week_01/leetcode/editor/cn/lesson3/practice/ReverseNodesInKGroup.java
+++ Week_01/leetcode/editor/cn/lesson3/practice/ReverseNodesInKGroup.java
@@ -30,23 +30,54 @@ package leetcode.editor.cn.lesson3.practice;
 public class ReverseNodesInKGroup {
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
     /**
      * Definition for singly-linked list.
      * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode(int x) { val = x; }
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
      * }
      */
     class Solution {
         public ListNode reverseKGroup(ListNode head, int k) {
-            return null;
+            if (head == null || head.next == null || k == 1) return head;
+            ListNode dummyHead = new ListNode(-1);
+            dummyHead.next = head;
+            ListNode begin = dummyHead;
+            int i = 0;
+            while (head != null) {
+                i++;
+                if (i % k == 0) {
+                    begin = reverse(begin, head.next);
+                    head = begin.next;
+                } else {
+                    head = head.next;
+                }
+            }
+            return dummyHead.next;
         }
+    }
+
+    public ListNode reverse(ListNode begin, ListNode end) {
+        ListNode curr = begin.next;
+        ListNode prev = begin;
+        ListNode first = curr;
+        ListNode next;
+        while (curr != end) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        begin.next = prev;
+        first.next = curr;
+        return first;
     }
 
     class ListNode {
         int val;
-        SwapNodesInPairs.ListNode next;
+        ListNode next;
         public ListNode(int val) {
             this.val = val;
         }
