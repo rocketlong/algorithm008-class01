5a37a221c2b9f74ad6653b5996996bc47088cd04
liyunlong <383034700@qq.com>
2020-05-17 18:10:00 +0800

    二分

---
+++ Week_04/leetcode/editor/cn/lesson10/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,49 @@
+//假设按照升序排序的数组在预先未知的某个点上进行了旋转。 
+//
+// ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。 
+//
+// 请找出其中最小的元素。 
+//
+// 你可以假设数组中不存在重复元素。 
+//
+// 示例 1: 
+//
+// 输入: [3,4,5,1,2]
+//输出: 1 
+//
+// 示例 2: 
+//
+// 输入: [4,5,6,7,0,1,2]
+//输出: 0 
+// Related Topics 数组 二分查找
+
+package leetcode.editor.cn.lesson10;
+
+//Java：寻找旋转排序数组中的最小值
+public class FindMinimumInRotatedSortedArray {
+    public static void main(String[] args) {
+        Solution solution = new FindMinimumInRotatedSortedArray().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findMin(int[] nums) {
+            int left = 0, right = nums.length - 1;
+            int min = Integer.MAX_VALUE;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[0] <= nums[mid]) {
+                    min = Math.min(nums[0], min);
+                    left = mid + 1;
+                } else if (nums[0] > nums[mid]) {
+                    min = Math.min(nums[mid], min);
+                    right = mid - 1;
+                }
+            }
+            return min;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
