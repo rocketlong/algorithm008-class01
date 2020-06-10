6845b3fb9e016162b3f99d4371d92b81d1c31700
liyunlong <383034700@qq.com>
2020-06-11 00:40:29 +0800

    edit

--- Week_06/leetcode/editor/cn/lesson12/practice/BestTimeToBuyAndSellStockIii.java
+++ Week_06/leetcode/editor/cn/lesson12/practice/BestTimeToBuyAndSellStockIii.java
@@ -35,13 +35,29 @@ import com.sun.org.apache.regexp.internal.RE;
 public class BestTimeToBuyAndSellStockIii {
     public static void main(String[] args) {
         Solution solution = new BestTimeToBuyAndSellStockIii().new Solution();
-        // TO TEST
+        int[] prices = {3,3,5,0,0,3,1,4};
+        System.out.println(solution.maxProfit(prices));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maxProfit(int[] prices) {
-            return 0;
+            if (prices == null || prices.length == 0) return 0;
+            int[][][] dp = new int[prices.length][3][2];
+            dp[0][0][0] = 0;
+            dp[0][1][0] = Integer.MIN_VALUE;
+            dp[0][2][0] = Integer.MIN_VALUE;
+            dp[0][0][1] = -prices[0];
+            dp[0][1][1] = Integer.MIN_VALUE;
+            dp[0][2][1] = Integer.MIN_VALUE;
+            for (int i = 1; i < prices.length; i++) {
+                dp[i][0][0] = dp[i - 1][0][0];
+                dp[i][0][1] = Math.max(dp[i - 1][0][1], dp[i - 1][0][0] - prices[i]);
+                dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][0][1] + prices[i]);
+                dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][1][0] == Integer.MIN_VALUE ? Integer.MIN_VALUE : dp[i - 1][1][0] - prices[i]);
+                dp[i][2][0] = Math.max(dp[i - 1][2][0], dp[i - 1][1][1] + prices[i]);
+            }
+            return Math.max(Math.max(dp[prices.length - 1][0][0], dp[prices.length - 1][1][0]), dp[prices.length - 1][2][0]);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
