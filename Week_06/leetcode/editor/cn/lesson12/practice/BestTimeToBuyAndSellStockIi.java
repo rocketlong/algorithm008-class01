3e4de34344647890fba8aa7dc1249c97d0a3802f
liyunlong <383034700@qq.com>
2020-07-26 23:25:30 +0800

    edit

--- Week_06/leetcode/editor/cn/lesson12/practice/BestTimeToBuyAndSellStockIi.java
+++ Week_06/leetcode/editor/cn/lesson12/practice/BestTimeToBuyAndSellStockIi.java
@@ -64,17 +64,58 @@ public class BestTimeToBuyAndSellStockIi {
 //        }
 
         // dp
+//        public int maxProfit(int[] prices) {
+//            if (prices == null || prices.length == 0) return 0;
+//            int m = prices.length;
+//            int[][] dp = new int[m][2]; // 0-没有持有股票 1-持有股票
+//            dp[0][0] = 0;
+//            dp[0][1] = -prices[0];
+//            for (int i = 1; i < m; i++) {
+//                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+//                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+//            }
+//            return Math.max(dp[m - 1][0], dp[m - 1][1]);
+//        }
+
+        // dp（套路）
+//        public int maxProfit(int[] prices) {
+//            if (prices == null || prices.length == 0) return 0;
+//            int n = prices.length;
+//            int[][] dp = new int[n][2];
+//            for (int i = 0; i < n; i++) {
+//                if (i - 1 == -1) {
+//                    dp[i][0] = 0;
+//                    dp[i][1] = -prices[0];
+//                    continue;
+//                }
+//                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+//                /**
+//                 * 动态转移方程：
+//                 * dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+//                 * dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+//                 *
+//                 * k = +infinity 则可以认为 k-1 = k
+//                 * 化简后动态转移方程：
+//                 * dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+//                 * dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+//                 */
+//                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+//            }
+//            return dp[n - 1][0];
+//        }
+
+        // dp（套路）优化为空间复杂度为O(1)
         public int maxProfit(int[] prices) {
             if (prices == null || prices.length == 0) return 0;
-            int m = prices.length;
-            int[][] dp = new int[m][2]; // 0-没有持有股票 1-持有股票
-            dp[0][0] = 0;
-            dp[0][1] = -prices[0];
-            for (int i = 1; i < m; i++) {
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            int n = prices.length;
+            int dp_i_0 = 0;
+            int dp_i_1 = Integer.MIN_VALUE;
+            for (int i = 0; i < n; i++) {
+                int temp = dp_i_0;
+                dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+                dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
             }
-            return Math.max(dp[m - 1][0], dp[m - 1][1]);
+            return dp_i_0;
         }
 
     }
